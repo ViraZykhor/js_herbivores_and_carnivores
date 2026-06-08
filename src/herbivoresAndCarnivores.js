a302ad5a77959ac9494dbab5b28b535acd428408
Vira Shmyhelska <vira.zykhor@gmail.com>
2026-06-09 01:08:40 +0300

    add filter

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -28,10 +28,7 @@ class Carnivore extends Animal {
     if (herbivore instanceof Herbivore && !herbivore.hidden) {
       herbivore.health -= 50;
     }
-
-    if (herbivore.health <= 0) {
-      Animal.alive.splice(Animal.alive.indexOf(herbivore), 1);
-    }
+    Animal.alive = Animal.alive.filter((animal) => animal.health > 0);
   }
 }
 
